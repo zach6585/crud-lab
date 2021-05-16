de5acb4e66e64808f271198fda8dbaee82b34d63
zach6585 <zmines@wesleyan.edu>
2021-05-16 14:18:09 -0400

    commit tree c44cdb72e6c6c0e7b2a9ba5d4cc318aa3698b2ae

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 class RestaurantInput extends Component {
+  
+  state = {
+    text: ''
+  }
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value 
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state)
+    this.setState({text: ''})
+  }
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={event => this.handleChange(event)}></input>
+          <input type="submit">Submit</input>
+        </form>
       </div>
     );
   }
 };
 
-export default RestaurantInput;
+const mapDispatchToProps = (dispatch) => ({
+  addRestaurant: inputData => dispatch({type: 'ADD_RESTAURANT', payload: inputData})
+})
+
+export default connect(null, mapDispatchToProps)(RestaurantInput);
